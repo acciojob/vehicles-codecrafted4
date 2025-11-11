83cca9adca2c681f0931e647c6c4ded3f6858680
UTKARSH MISHRA <143809298+codecrafted4@users.noreply.github.com>
2025-11-11 14:06:20 +0530

    Refactor accelerate method in F1 class
    Fix newSpeed calculation and adjust gear changes based on speed.

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,4,6,isManual,"SUV",5);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+      int newSpeed=0 ;//set the value of new speed by using currentSpeed and rate
+        newSpeed = getSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,11 +19,34 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
-        //for all other cases, change the gear accordingly
+        else if(newSpeed>=1 && newSpeed<=50){
+            changeGear(1);
+        }
+        else if(newSpeed>=51 && newSpeed<=100){
+            changeGear(2);
+        }
+        else if(newSpeed>=101 && newSpeed<=150){
+            changeGear(3);
+        }
+        else if(newSpeed>=151 && newSpeed<=200){
+            changeGear(4);
+        }
+        else if(newSpeed>=201 && newSpeed<=250){
+            changeGear(5);
+        }
+        else if(newSpeed>250){
+            changeGear(6);
+        }
+        else{
+            changeGear(-1);
+            steer(180);
+        }
+        //for all other cases, change the gear accordingl
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
