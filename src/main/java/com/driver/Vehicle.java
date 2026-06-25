5b692cb446852ae5dfdac644397be08e435fc3b6
Utkarsh Mishra <utkarshmishra171198@gmail.com>
2026-06-25 13:00:32 +0530

    updated Vehicle implementation

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -24,7 +24,7 @@ public class Vehicle {
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
-        this.currenSpeed = speed;
+        this.currentSpeed = speed;
         this.currentDirection = direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
